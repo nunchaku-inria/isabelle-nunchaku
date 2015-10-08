e2796125e8d409c9e6a52c9830663a1242be2713
blanchet <none@none>
2015-10-08 22:56:48 +0200

    tuning

--- Nunchaku.thy
+++ Nunchaku.thy
@@ -2,6 +2,11 @@ theory Nunchaku
 imports Main
 begin
 
+ML_file "Tools/nunchaku_util.ML"
+ML_file "Tools/nunchaku_tool.ML"
+ML_file "Tools/nunchaku_problem.ML"
+ML_file "Tools/nunchaku_model.ML"
 ML_file "Tools/nunchaku.ML"
+ML_file "Tools/nunchaku_commands.ML"
 
 end
