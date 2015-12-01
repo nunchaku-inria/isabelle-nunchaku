0d820b1be5b792e2fa29c3cf8d331772c1305e99
blanchet <none@none>
2015-12-01 13:38:44 +0100

    monomorphize datatypes

--- Nunchaku.thy
+++ Nunchaku.thy
@@ -14,7 +14,7 @@ ML_file "Tools/nunchaku_model.ML"
 ML_file "Tools/nunchaku.ML"
 ML_file "Tools/nunchaku_commands.ML"
 
-datatype natopt = None | Some nat
+(* datatype natopt = None | Some nat *)
 
 lemma "x = None"
 nunchaku[overlord]
