07db29796c48503cf753a9ff01d3d7f30d46b9c3
blanchet <none@none>
2016-01-29 09:27:43 +0100

    ugly model etc. + copyright year

--- Nunchaku.thy
+++ Nunchaku.thy
@@ -11,12 +11,18 @@ definition rmember :: "'a set \<Rightarrow> 'a \<Rightarrow> bool" where
 ML_file "Tools/nunchaku_util.ML"
 ML_file "Tools/nunchaku_collect.ML"
 ML_file "Tools/nunchaku_problem.ML"
-ML_file "Tools/nunchaku_tool.ML"
 ML_file "Tools/nunchaku_translate.ML"
 ML_file "Tools/nunchaku_model.ML"
+ML_file "Tools/nunchaku_reconstruct.ML"
+ML_file "Tools/nunchaku_display.ML"
+ML_file "Tools/nunchaku_tool.ML"
 ML_file "Tools/nunchaku.ML"
 ML_file "Tools/nunchaku_commands.ML"
 
 hide_const (open) rmember
+
+lemma "hd xs = [x]"
+nunchaku
+oops
 
 end
