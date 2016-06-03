5689f4b130d28884bd2d517ca13f416883bc2346
blanchet <none@none>
2016-06-04 00:29:21 +0200

    more nice priting

--- Nunchaku.thy
+++ Nunchaku.thy
@@ -27,16 +27,4 @@ ML_file "Tools/nunchaku_commands.ML"
 
 hide_const (open) The_unsafe unreachable rmember
 
-declare [[ML_exception_trace]]
-
-lemma "(undefined :: ('a \<Rightarrow> 'a list) \<Rightarrow> bool) x"
-using [[show_types]]
-nunchaku[overlord]
-
-
-(*
-lemma "x + y = y + x \<and> rev xs = xs"
-nunchaku[overlord]
-*)
-
 end
