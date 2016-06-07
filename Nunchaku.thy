625b736d8d37cad0a8de84134cd4c3f9afb9a294
blanchet <none@none>
2016-06-08 00:46:08 +0200

    a solution to whackiness

--- Nunchaku.thy
+++ Nunchaku.thy
@@ -5,11 +5,10 @@ keywords
   "nunchaku_params" :: thy_decl
 begin
 
+consts unreachable :: 'a
+
 definition The_unsafe :: "('a \<Rightarrow> bool) \<Rightarrow> 'a" where
   "The_unsafe = The"
-
-definition unreachable :: 'a where
-  "unreachable = The_unsafe (\<lambda>_. False)"
 
 definition rmember :: "'a set \<Rightarrow> 'a \<Rightarrow> bool" where
   "rmember A x \<longleftrightarrow> x \<in> A"
@@ -25,19 +24,6 @@ ML_file "Tools/nunchaku_tool.ML"
 ML_file "Tools/nunchaku.ML"
 ML_file "Tools/nunchaku_commands.ML"
 
-codatatype 'a llist = LNil | LCons 'a "'a llist"
-
-(*
-lemma "xs = LCons a ys \<Longrightarrow> ys = LCons b xs \<Longrightarrow> xs = ys"
-nunchaku[overlord, debug]
-*)
-
-(*
-(* FIXME: whack with "unreachable" is not a good idea *)
-lemma "rev xs = xs \<or> xs = ys"
-nunchaku[satisfy, whack rev, debug]
-*)
-
-hide_const (open) The_unsafe unreachable rmember
+hide_const (open) unreachable The_unsafe rmember
 
 end
